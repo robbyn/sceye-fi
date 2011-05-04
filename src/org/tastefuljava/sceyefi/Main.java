b92a86b7f367bd3cbb4ce77b492a1a04f59930ae
robbyns1@gmail.com <robbyns1@gmail.com@a0b6377e-93db-b975-94d8-56ad6f9c393a>
2011-05-04 09:01:02 +0000

    Initial commit

--- src/org/tastefuljava/sceyefi/Main.java
+++ src/org/tastefuljava/sceyefi/Main.java
@@ -1,5 +1,6 @@
 package org.tastefuljava.sceyefi;
 
+import java.io.File;
 import org.tastefuljava.sceyefi.conf.EyeFiConf;
 import org.tastefuljava.sceyefi.server.EyeFiServer;
 import java.io.IOException;
@@ -7,13 +8,41 @@ import java.util.logging.Level;
 import java.util.logging.Logger;
 
 public class Main {
+    private static File settings = null;
 
     public static void main(String[] args) {
         try {
-            EyeFiConf conf = EyeFiConf.load();
+            if (!parseArgs(args)) {
+                usage();
+                return;
+            }
+            EyeFiConf conf;
+            if (settings == null) {
+                conf = EyeFiConf.load();
+            } else {
+                conf = EyeFiConf.load(settings);
+            }
             EyeFiServer.start(conf);
         } catch (IOException ex) {
             Logger.getLogger(Main.class.getName()).log(Level.SEVERE, null, ex);
         }
+    }
+
+    private static boolean parseArgs(String[] args) {
+        int i = 0;
+        while (i < args.length) {
+            String arg = args[i++];
+            if (arg.equals("-settings") && i < args.length) {
+                settings = new File(args[i++]);
+            } else {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    private static void usage() {
+        System.out.println(
+                "Usage: java -jar sceye-fi.jar [-settings <settings-file>]");
     }
 }
